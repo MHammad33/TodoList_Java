1b63be12e8233b31ac1cb0a22c51a3b551055708
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2023-12-27 14:14:59 +0500

    Created some functions in the footer for later use

--- src/AppFrame.java
+++ src/AppFrame.java
@@ -1,19 +1,29 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.BorderLayout;
 
 public class AppFrame extends JFrame {
     private TitleBar titleBar;
     private Footer footer;
+
+    private JButton newTask;
+    private JButton clear;
+
     public AppFrame(){
+        // Creating the basic frame
         this.setSize(400, 700);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
 
+        // Creating title bar and footer
         titleBar = new TitleBar();
         footer = new Footer();
 
-
+        // Adding title bar and footer in the frame
         this.add(titleBar, BorderLayout.NORTH);
         this.add(footer, BorderLayout.SOUTH);
+
+        // Get the functionalities of footer buttons to these variables
+        newTask = footer.getNewTask();
+        clear = footer.getClear();
     }
 }
--- src/Footer.java
+++ src/Footer.java
@@ -5,25 +5,35 @@ import java.awt.*;
 public class Footer extends JPanel {
         JButton addTask;
         JButton clear;
-
         Border emptyBorder = BorderFactory.createEmptyBorder();
-    public Footer(){
-        this.setPreferredSize(new Dimension(400, 60));
 
-        addTask = new JButton("Add Task");
-        addTask.setBorder(emptyBorder);
-        addTask.setFont(new Font("Sans-serif", Font.PLAIN, 20));
-        addTask.setVerticalAlignment(JButton.BOTTOM);
-        addTask.setBackground(Color.white);
-        this.add(addTask);
+        public Footer(){
+            this.setPreferredSize(new Dimension(400, 60));
 
-        // For space between buttons
-        this.add(Box.createHorizontalStrut(20));
+            // Add Task Button Styling
+            addTask = new JButton("Add Task");
+            addTask.setBorder(emptyBorder);
+            addTask.setFont(new Font("Sans-serif", Font.PLAIN, 20));
+            addTask.setVerticalAlignment(JButton.BOTTOM);
+            addTask.setBackground(Color.white);
+            this.add(addTask);
 
-        clear = new JButton("Clear Finished Task");
-        clear.setBorder(emptyBorder);
-        clear.setFont(new Font("Sans-serif", Font.PLAIN, 20));
-        clear.setBackground(Color.white);
-        this.add(clear);
-    }
+            // For space between buttons
+            this.add(Box.createHorizontalStrut(20));
+
+            // Clear Button Styling
+            clear = new JButton("Clear Finished Task");
+            clear.setBorder(emptyBorder);
+            clear.setFont(new Font("Sans-serif", Font.PLAIN, 20));
+            clear.setBackground(Color.white);
+            this.add(clear);
+        }
+
+        public JButton getNewTask(){
+            return addTask;
+        }
+
+        public JButton getClear(){
+            return clear;
+        }
 }
