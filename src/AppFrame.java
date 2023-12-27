c78832552246f0aa0013dc2a09403bdda3f32c08
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2023-12-27 13:46:56 +0500

    Created a Simple Frame with default functionalities

---
+++ src/AppFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class AppFrame extends JFrame {
+    public AppFrame(){
+        this.setSize(400, 700);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
